72657c76e58576f9003a8f1081003365967e4851
thiagoferr <thiago1729@gmail.com>
2022-11-14 16:29:25 -0300

    deploy-movies

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ const express_1 = __importDefault(require("express"));
 const index_1 = require("./routes/index");
 const database_1 = require("./database");
 const app = (0, express_1.default)();
-const port = 3333;
 (0, database_1.hasConnection)();
 // const routes = require('./routes');
 app.use(express_1.default.json());
 app.use(index_1.routes);
-app.listen(port, () => {
-    console.log("listening on port " + port);
-});
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
